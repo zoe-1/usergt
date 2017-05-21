d39074f756114f80c99f7728d841190b6a3ace3a
Jon Swenson <jswenson74@gmail.com>
2017-05-21 18:56:20 -0400

    Built create & destroy
    * refactor file names
    * 100% coverage for create & destroy logic

---
+++ __tests__/gt/destroy.test.js
@@ -0,0 +1,158 @@
+'use strict';
+
+
+const Penseur = require('penseur');
+const Usergt = require('../../lib');
+
+const Config = {
+    dbname: 'usergt',
+    connection: {
+        host: 'localhost',
+        port: 28015
+    },
+    state: 'live',      //  live | dev defaults live.
+    purge: 'on',        //  default is 'off'.
+    admin: true         //  Boolean default false.
+};
+
+
+describe('destroy', () => {
+
+    it('destroy document', (done) => {
+
+        const usergt = new Usergt.Gt(Config);
+
+        const newUserRecord = {
+            username: 'zoelogic',
+            email: 'js@zoelogic.com',
+            password: 'paSS-w0rd_4test',
+            scope: ['user']
+        };
+
+        usergt.create(newUserRecord, (err, newUserId) => {
+
+            const ConfigObject = require('../../lib/config');
+
+            ConfigObject.purge = 'off';
+            expect(err).toBe(null);
+
+            usergt.destroy(newUserId, (err, result) => {
+
+                ConfigObject.purge = 'on';
+                return done();
+            });
+        });
+    });
+
+    it('Query.user.destroy fail', (done) => {
+
+        const usergt = new Usergt.Gt(Config);
+
+        const newUserRecord = {
+            username: 'zoelogic',
+            email: 'js@zoelogic.com',
+            password: 'paSS-w0rd_4test',
+            scope: ['user']
+        };
+
+        usergt.create(newUserRecord, (err, newUserId) => {
+
+            const ConfigObject = require('../../lib/config');
+
+            ConfigObject.purge = 'off';
+
+            expect(err).toBe(null);
+
+            const original = ConfigObject.connection.host;
+            ConfigObject.connection.host = 'boomhost';
+
+            usergt.destroy(newUserId, (err, result) => {
+
+                ConfigObject.connection.host = original;
+                ConfigObject.purge = 'on';
+                expect(err.message).toBe('Penseur establish failed');
+                return done();
+            });
+        });
+    });
+
+    it('mock db.user.update fail - no document found', (done) => {
+
+        const usergt = new Usergt.Gt(Config);
+
+        const newUserId = 33;
+
+        usergt.destroy(newUserId, (err, result) => {
+
+            expect(err.message).toBe('No document found');
+            return done();
+        });
+    });
+
+    it('mock db.user.update fail - no document found', (done) => {
+
+        const usergt = new Usergt.Gt(Config);
+
+        const newUserId = 33;
+
+        const Table = require('../../lib/table');
+
+        const Override = class extends Penseur.Table {  // use override to mock err
+            update(documentId, unsets, callback) {
+
+                return callback(new Error('db.user.update failed'));
+            }
+        };
+
+        Table[Config.purge].user.extended = Override;
+
+        usergt.destroy(newUserId, (err, result) => {
+
+            delete Table[Config.purge].user.extended;
+            expect(err.message).toBe('destroy failed unset');
+            return done();
+        });
+    });
+
+    it('mock db.user.remove fail', (done) => {
+
+        const usergt = new Usergt.Gt(Config);
+
+        const newUserRecord = {
+            username: 'zoelogic',
+            email: 'js@zoelogic.com',
+            password: 'paSS-w0rd_4test',
+            scope: ['user']
+        };
+
+        const ConfigObject = require('../../lib/config');
+
+        ConfigObject.purge = 'on';
+
+        const Table = require('../../lib/table');
+
+        const Override = class extends Penseur.Table {  // use override to mock err
+            remove(documentId, callback) {
+
+                return callback(new Error('db.user.remove failed'));
+            }
+        };
+
+        Table['off'].user.extended = Override;
+
+        usergt.create(newUserRecord, (err, newUserId) => {
+
+            ConfigObject.purge = 'off';
+
+            expect(err).toBe(null);
+
+            usergt.destroy(newUserId, (err, result) => {
+
+                delete Table[Config.purge].user.extended;
+                ConfigObject.purge = 'on';
+                expect(err.message).toBe('destroy failed to remove');
+                return done();
+            });
+        });
+    });
+});
--- __tests__/gt/index.test.js
+++ __tests__/gt/index.test.js
@@ -19,7 +19,7 @@ const Config = {
 
 describe('lib/index', () => {
 
-    it('start usergt', () => {
+    it('start usergt', (done) => {
 
         const usergt = new User.Gt(Config);
 
@@ -28,9 +28,10 @@ describe('lib/index', () => {
 
         const ConfigFile = require('../../lib/config');
         expect(usergt._settings.connection.port).toBe(ConfigFile.connection.port);
+        done();
     });
 
-    it('start usergt - invalid configs', () => {
+    it('start usergt - invalid configs', (done) => {
 
         const original = Config.dbname;
         delete Config.dbname;
@@ -40,11 +41,13 @@ describe('lib/index', () => {
         } catch (error) {
             Config.dbname = original;
             expect(error.name).toBe('ValidationError');
+            return done();
         }
     });
 
     it('start usergt - create loaded', (done) => {
 
+        console.log(JSON.stringify(Config));
         const usergt = new User.Gt(Config);
 
         const newUserRecord = {
@@ -54,13 +57,13 @@ describe('lib/index', () => {
             scope: ['user']
         };
 
-        usergt.create(newUserRecord, (err, result) => {
+        return usergt.create(newUserRecord, (err, result) => {
         
-            // console.log('watch2 ' + Object.keys(this));
-            // console.log('user record: err    ' + err);
-            // console.log('user record: result ' + result);
-            expect(result).toBe('completed items');
-            done();
+            console.log(JSON.stringify(err, null, '\t'));
+            console.log(JSON.stringify(result, null, '\t'));
+            expect(err).toBe(null);
+            expect(result.length).toBe(36);
+            return done();
         });
     });
 
@@ -169,6 +172,169 @@ describe('lib/index', () => {
         usergt.create(newUserRecord, (err, result) => {
         
             expect(err.message).toBe('bcrypt hash failed');
+            done();
+        });
+    });
+
+    it('mock Query.user.create err', (done) => {
+
+        const User = require('../../lib');
+
+        const original = User.Query.user.create;
+
+        User.Query.user.create = function (userRecord, callback) {
+
+            User.Query.user.create = original;
+            return callback(new Error('mock Query.user.create failure'), null);
+        };
+
+        const usergt = new User.Gt(Config);
+
+        const newUserRecord = {
+            username: 'zoelogic',
+            email: 'js@zoelogic.com',
+            password: 'paSS-w0rd_4test',
+            scope: ['user']
+        };
+
+        usergt.create(newUserRecord, (err, result) => {
+        
+            expect(err.message).toBe('mock Query.user.create failure');
+            done();
+        });
+    });
+
+    it('mock Query.user.create db.establish failure', (done) => {
+
+        const User = require('../../lib');
+
+        const original = Config.connection.host;
+
+        Config.connection.host = 'boomhost';
+
+        const usergt = new User.Gt(Config);
+
+        const newUserRecord = {
+            username: 'zoelogic',
+            email: 'js@zoelogic.com',
+            password: 'paSS-w0rd_4test',
+            scope: ['user']
+        };
+
+        usergt.create(newUserRecord, (err, result) => {
+        
+            // console.log('Error Message: ' + err.message);
+            Config.connection.host = original;
+            // expect(err.message).toBe('mock Query.user.create failure');
+            done();
+        });
+    });
+
+    it('mock Query.user.create violates unique restriction', (done) => {
+
+        const User = require('../../lib');
+
+        const original = Config.connection.host;
+
+        // Config.connection.host = 'boomhost';
+        // console.log(JSON.stringify(Config));
+        // console.log(Object.keys(usergt));
+
+        const usergt1 = new User.Gt(Config);
+
+        const newUserRecord = {
+            username: 'zoelogic',
+            email: 'js@zoelogic.com',
+            password: 'paSS-w0rd_4test',
+            scope: ['user']
+        };
+
+        return usergt1.create(newUserRecord, (err, result) => {
+        
+            expect(err).toBe(null);
+
+            Config.purge = 'off';
+
+            const usergt2 = new User.Gt(Config);
+
+            const newUserRecord2 = {
+                username: 'zoelogic',
+                email: 'js@zoelogic.com',
+                password: 'paSS-w0rd_4test',
+                scope: ['user']
+            };
+
+            return usergt2.create(newUserRecord2, (err, result) => {
+            
+                Config.purge = 'on';
+                expect(err.message).toBe('uniqueness violation');
+                expect(err.output.payload.statusCode).toBe(400);
+                expect(err.output.payload.error).toBe('Bad Request');
+                return done();
+            });
+        });
+    });
+
+    it('mock Query.user.create db.user.insert error (extended)', (done) => {
+
+        const Penseur = require('penseur');
+
+        const Table = require('../../lib/table');
+
+        const Override = class extends Penseur.Table {  // use override to mock err
+            insert(userRecord, callback) {
+
+                return callback(new Error('db.user.insert failed'));
+            }
+        };
+
+        Table[Config.purge].user.extended = Override;
+
+        const User = require('../../lib');
+
+        const usergt = new User.Gt(Config);
+
+        const newUserRecord = {
+            username: 'zoelogic',
+            email: 'js@zoelogic.com',
+            password: 'paSS-w0rd_4test',
+            scope: ['user']
+        };
+
+        usergt.create(newUserRecord, (err, result) => {
+        
+            delete Table[Config.purge].user.extended; 
+            expect(err.output.payload.statusCode).toBe(500);
+            expect(err.output.payload.error).toBe('Internal Server Error');
+            expect(err.message).toBe('insert failed');
+            done();
+        });
+    });
+
+    it('mock Query.user.create err', (done) => {
+
+        const User = require('../../lib');
+
+        const original = User.Query.user.create;
+
+        User.Query.user.create = function (userRecord, callback) {
+
+            User.Query.user.create = original;
+            return callback(new Error('mock Query.user.create failure'), null);
+        };
+
+        const usergt = new User.Gt(Config);
+
+        const newUserRecord = {
+            username: 'zoelogic',
+            email: 'js@zoelogic.com',
+            password: 'paSS-w0rd_4test',
+            scope: ['user']
+        };
+
+        usergt.create(newUserRecord, (err, result) => {
+        
+            expect(err.message).toBe('mock Query.user.create failure');
             done();
         });
     });
