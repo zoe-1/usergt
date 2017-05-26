e33d1fa57ba42d564c4f236564b6d09957879584
Jon Swenson <jswenson74@gmail.com>
2017-05-26 19:08:43 -0400

    working on coverage for authenticate

--- __tests__/gt/authenticate.test.js
+++ __tests__/gt/authenticate.test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Hoek = require('hoek');
+const Items = require('items');
 const Penseur = require('penseur');
 const Usergt = require('../../lib');
 
@@ -14,11 +16,13 @@ const Config = {
     admin: true         //  Boolean default false.
 };
 
+const internals = {};
+
 describe('authenticate', () => {
 
     beforeEach((done) => {
 
-        // beforeEach test 
+        // beforeEach test
         // * purge purge database
         // * create newUserRecord to authenticate against
 
@@ -35,7 +39,7 @@ describe('authenticate', () => {
             const usergt = new Usergt.Gt(Config);
 
             usergt.establish((err) => {
-                
+
                 const newUserRecord = {
                     username: 'zoelogic',
                     email: 'js@zoelogic.com',
@@ -52,32 +56,23 @@ describe('authenticate', () => {
         });
     });
 
-    it('authenticate document', (done) => {
+    it('authenticate success', (done) => {
 
         const usergt = new Usergt.Gt(Config);
-
-        const newUserRecord = {
-            username: 'zoelogic',
-            email: 'js@zoelogic.com',
-            password: 'paSS-w0rd_4test',
-            scope: ['user']
-        };
 
         usergt.establish((err) => {
 
             expect(err).toBe(null);
-        
+
             const username = 'zoelogic';
             const password = 'paSS-w0rd_4test';
 
+            usergt.authenticate(username, password, (err, authenticedUserRecord) => {
 
-            usergt.authenticate(username, password, (err, authenticUserRecord) => {
-
-                // expect(err).toBe(null);
-
-                // console.log('authenticUserRecord: ' + JSON.stringify(authenticUserRecord, null, '\t'));
+                expect(authenticedUserRecord.username).toBe('zoelogic');
+                expect(authenticedUserRecord.loginAttempts).toBe(0);
+                expect(authenticedUserRecord.lockUntil).toBeLessThan(Date.now());
                 return usergt.db.close(done);
-                // return done();
             });
         });
     });
@@ -101,7 +96,7 @@ describe('authenticate', () => {
         });
     });
 
-    it('password does not exist', (done) => {
+    it('password d/n exist', (done) => {
 
         const usergt = new Usergt.Gt(Config);
 
@@ -114,13 +109,17 @@ describe('authenticate', () => {
 
                 expect(err.output.statusCode).toBe(401);
                 expect(err.output.payload.error).toBe('Unauthorized');
-                expect(err.message).toBe('password does not match');
+                expect(err.message).toBe('password incorrect');
                 return usergt.db.close(done);
             });
         });
     });
 
     it('invalid password', (done) => {
+
+        // difference between password not existing versus invalid is
+        // usergt first filters invalid passwords/usernames out before making any queries
+        // to the database.
 
         const usergt = new Usergt.Gt(Config);
 
@@ -131,9 +130,9 @@ describe('authenticate', () => {
 
             usergt.authenticate(username, password, (err, authenticatedUserRecord) => {
 
-                expect(err.output.statusCode).toBe(422);
-                expect(err.output.payload.error).toBe('Unprocessable Entity');
-                expect(err.output.payload.message).toBe('Joi password validation failed');
+                expect(err.output.statusCode).toBe(401);
+                expect(err.output.payload.error).toBe('Unauthorized');
+                expect(err.message).toBe('password incorrect');
                 return usergt.db.close(done);
             });
         });
@@ -150,14 +149,19 @@ describe('authenticate', () => {
 
             usergt.authenticate(username, password, (err, authenticatedUserRecord) => {
 
-                expect(err.output.statusCode).toBe(422);
-                expect(err.output.payload.error).toBe('Unprocessable Entity');
-                expect(err.output.payload.message).toBe('Joi username validation failed');
+                // invalid data gets unauthorized response too.
+                // Just filter it out early by deterimining it is invalid.
+                // Goal is to save a hit to the database.
+
+                expect(err.output.statusCode).toBe(401);
+                expect(err.output.payload.error).toBe('Unauthorized');
+                expect(err.message).toBe('username incorrect');
+                
                 return usergt.db.close(done);
             });
         });
     });
- 
+
     it('Query.user.findByUsername db.user.query failure', (done) => {
 
         Config.connection.test = true;
@@ -167,13 +171,13 @@ describe('authenticate', () => {
         usergt.establish((err) => {
 
             usergt.db.disable('user', 'query', { value: new Error('query failed') });
-        
+
             const username = 'zoelogic';
             const password = 'paSS-w0rd_4test';
 
             usergt.authenticate(username, password, (err, authenticatedUserRecord) => {
 
-                delete Config.connection.test; 
+                delete Config.connection.test;
                 usergt.db.enable('user', 'query');
 
                 expect(err.output.statusCode).toBe(500);
@@ -193,7 +197,7 @@ describe('authenticate', () => {
         usergt.establish((err) => {
 
             usergt.connected = false;  // mocks disconnected setting
-        
+
             const username = 'zoelogic';
             const password = 'paSS-w0rd_4test';
 
@@ -206,101 +210,274 @@ describe('authenticate', () => {
             });
         });
     });
+});
 
-// 
-//     it('username invalid', (done) => {
-//     
-//         const usergt = new Usergt.Gt(Config);
-// 
-//         const newUserRecord = {
-//             username: 'zoelogic',
-//             email: 'js@zoelogic.com',
-//             password: 'paSS-w0rd_4test',
-//             scope: ['user']
-//         };
-// 
-//         usergt.create(newUserRecord, (err, newUserId) => {
-// 
-//             expect(err).toBe(null);
-// 
-//             const ConfigObject = require('../../lib/config');
-//             ConfigObject.purge = 'off';
-// 
-//             const username = '';
-//             const password = 'paSS-w0rd_4test';
-//         
-//             usergt.authenticate(username, password, (err, authenticUserRecord) => {
-//             
-//                 ConfigObject.purge = 'on';
-//                 expect(err.output.payload.message).toBe('Joi username validation failed');
-//                 return done();
-//             });
-//         });
-//     });
-// 
-//     it('password invalid', (done) => {
-//     
-//         const usergt = new Usergt.Gt(Config);
-// 
-//         const newUserRecord = {
-//             username: 'zoelogic',
-//             email: 'js@zoelogic.com',
-//             password: 'paSS-w0rd_4test',
-//             scope: ['user']
-//         };
-// 
-//         usergt.create(newUserRecord, (err, newUserId) => {
-// 
-//             expect(err).toBe(null);
-// 
-//             const ConfigObject = require('../../lib/config');
-//             ConfigObject.purge = 'off';
-// 
-//             const username = 'zoelogic';
-//             const password = 'paSS-w0rd_test';
-//         
-//             usergt.authenticate(username, password, (err, authenticUserRecord) => {
-//             
-//                 ConfigObject.purge = 'on';
-//                 expect(err.output.payload.message).toBe('Joi password validation failed');
-//                 return done();
-//             });
-//         });
-//     });
-// 
-// 
-//     it('Query.user.findByUsername db.establish fails', (done) => {
-//     
-//         const usergt = new Usergt.Gt(Config);
-// 
-//         const newUserRecord = {
-//             username: 'zoelogic',
-//             email: 'js@zoelogic.com',
-//             password: 'paSS-w0rd_4test',
-//             scope: ['user']
-//         };
-// 
-//         usergt.create(newUserRecord, (err, newUserId) => {
-// 
-//             expect(err).toBe(null);
-// 
-//             const ConfigObject = require('../../lib/config');
-//             ConfigObject.purge = 'off';
-//             ConfigObject.connection.host = 'boomboom';
-// 
-//             const username = 'zoelogic';
-//             const password = 'paSS-w0rd_4test';
-//         
-//             usergt.authenticate(username, password, (err, authenticatedUserRecord) => {
-//             
-//                 ConfigObject.purge = 'on';
-//                 ConfigObject.connection.host = 'localhost';
-//                 // console.log('authenticatedUserRecord:    err: ' + err);
-//                 // console.log('authenticatedUserRecord: result: ' + authenticatedUserRecord);
-//                 // expect(err.output.payload.message).toBe('');
-//                 expect(err.message).toBe('Penseur establish failed');
-//                 return done();
-//             });
-//         });
-//    });
+
+describe('lockout', () => {
+
+    it('cause lockout', (done) => {
+
+        // setup - previous test does prep work
+        //         so userRecord already exists 
+
+        const User = require('../../lib');
+
+        const usergt = new User.Gt(Config);
+
+        usergt.establish((err) => {
+
+            // ten bad login attempts
+
+            const username = 'zoelogic';
+            const password = 'paSS-w0rd_4t--t';
+
+            const badAttempt = function (item, next, i) {
+
+                usergt.authenticate(username, password, (err, authenticatedUserRecord) => {
+
+                    expect(err.output.statusCode).toBe(401);
+                    expect(err.output.payload.error).toBe('Unauthorized');
+                    if (item >= 10) {
+                        expect(err.message).toBe('password incorrect (started lockout)');
+                    } else {
+                        expect(err.message).toBe('password incorrect');
+                    }
+                    next();
+                });
+            };
+
+            Items.serial([1,2,3,4,5,6,7,8,9,10], badAttempt, (err) => {
+
+                expect(err).toBe(undefined);
+
+                User.Query.user.findByUsername.call(usergt, 'zoelogic', (err, userRecord) => {
+
+                    expect(err).toBe(null);
+
+                    const twentyThreeHours = (((1000 * 60) * 60) * 24) - 30000;  // 24 hours minus 30000 milliseconds
+                    const target = Date.now() + twentyThreeHours;
+
+                    expect(userRecord.lockUntil).toBeGreaterThan(target);
+                    expect(userRecord.loginAttempts).toBe(10);
+
+                    return usergt.db.close(done);
+                });
+            });
+        });
+    });
+
+    it('enforce lockout', (done) => {
+
+        const User = require('../../lib');
+
+        const usergt = new User.Gt(Config);
+
+        usergt.establish((err) => {
+
+            const username = 'zoelogic';
+            const password = 'paSS-w0rd_4test';
+
+            usergt.authenticate(username, password, (err, authenticUserRecord) => {
+
+                expect(err.output.statusCode).toBe(403);
+                expect(err.output.payload.error).toBe('Forbidden');
+                expect(err.message).toBe('account locked');
+                return usergt.db.close(done);
+            });
+        });
+    });
+
+    it('expire lockout', (done) => {
+
+        const User = require('../../lib');
+
+        const usergt = new User.Gt(Config);
+
+        usergt.establish((err) => {
+
+            User.Query.user.findByUsername.call(usergt, 'zoelogic', (err, userRecord) => {
+
+                internals.originalExpirationTime = userRecord.lockUntil;
+
+                const twentyThreeHours = (((1000 * 60) * 60) * 24) - 30000;  // 24 hours minus 30000 milliseconds
+                const target = Date.now() + twentyThreeHours;
+
+                // prove lockout in effect
+
+                expect(userRecord.lockUntil).toBeGreaterThan(target);
+                expect(userRecord.loginAttempts).toBe(10);
+
+                // expire the lockout
+
+                usergt.expireLockout(userRecord.id, (err) => {
+                
+                    expect(err).toBe(null);
+
+                    User.Query.user.findByUsername.call(usergt, 'zoelogic', (err, userRecord) => {
+
+                        expect(internals.originalExpirationTime).toBeGreaterThan(userRecord.lockUntil);
+                        expect(userRecord.lockUntil).toBeLessThan(Date.now());
+                        expect(userRecord.loginAttempts).toBe(10);
+                        return usergt.db.close(done);
+                    });
+                });
+            });
+        });
+    });
+
+    it('authenticate after lockout expiration - mock Query.user.resetLoginAttempt failure', (done) => {
+
+        const User = require('../../lib');
+
+        const usergt = new User.Gt(Config);
+
+        usergt.establish((err) => {
+
+            expect(err).toBe(null);
+
+            const original = User.Query.user.resetLoginAttempt;
+
+            User.Query.user.resetLoginAttempt = function (userId, callback) {
+            
+                User.Query.user.resetLoginAttempt = original;
+                return callback(new Error('resetLoginFailed'));
+            }; 
+
+            const username = 'zoelogic';
+            const password = 'paSS-w0rd_4test';
+
+            usergt.authenticate(username, password, (err, authenticatedUserRecord) => {
+
+                expect(err.output.statusCode).toBe(403);
+                expect(err.output.payload.error).toBe('Forbidden');
+                expect(err.message).toBe('account locked');
+                return usergt.db.close(done);
+            });
+        });
+    });
+
+    it('authenticate after lockout expiration', (done) => {
+
+        const User = require('../../lib');
+
+        const usergt = new User.Gt(Config);
+
+        usergt.establish((err) => {
+
+            expect(err).toBe(null);
+
+            const username = 'zoelogic';
+            const password = 'paSS-w0rd_4test';
+
+            usergt.authenticate(username, password, (err, authenticatedUserRecord) => {
+
+                expect(authenticatedUserRecord.username).toBe('zoelogic');
+                expect(authenticatedUserRecord.loginAttempts).toBe(0);
+                expect(authenticatedUserRecord.lockUntil).toBeLessThan(Date.now());
+                return usergt.db.close(done);
+            });
+        });
+    });
+});
+
+describe('coverage', () => {
+
+    beforeEach((done) => {
+
+        // beforeEach test
+        // * purge purge database
+        // * create newUserRecord to authenticate against
+
+        const prep = new Penseur.Db(Config.dbname, Config.connection);
+
+        const Tables = require('../../lib/table');
+
+        prep.establish(Tables['on'], (err) => {  // purge tables
+
+            expect(err).toBe(undefined);
+
+            prep.close();
+
+            const usergt = new Usergt.Gt(Config);
+
+            usergt.establish((err) => {
+
+                const newUserRecord = {
+                    username: 'zoelogic',
+                    email: 'js@zoelogic.com',
+                    password: 'paSS-w0rd_4test',
+                    scope: ['user']
+                };
+
+                usergt.create(newUserRecord, (err, newUserId) => {
+
+                    expect(err).toBe(null);
+                    usergt.db.close(done);
+                });
+            });
+        });
+    });
+
+    it('Query.user.resetLoginAttempt failure on passing authentication attempt', (done) => {
+
+        const User = require('../../lib');
+
+        const usergt = new User.Gt(Config);
+
+        usergt.establish((err) => {
+
+            expect(err).toBe(null);
+
+            const username = 'zoelogic';
+            const password = 'paSS-w0rd_4test';
+
+            const original = User.Query.user.resetLoginAttempt;
+
+            User.Query.user.resetLoginAttempt = function (userId, callback) {
+
+                User.Query.user.resetLoginAttempt = original;
+                return callback(new Error('mock resetLoginAttempt failure'), null); 
+            }; 
+
+            usergt.authenticate(username, password, (err, authenticatedUserRecord) => {
+
+                expect(err.message).toBe('mock resetLoginAttempt failure');
+                return usergt.db.close(done);
+            });
+        });
+    });
+
+    it('Query.user.incrementLoginAttempt db.user.update failure', (done) => {
+
+        Config.connection.test = true;
+
+        const usergt = new Usergt.Gt(Config);
+
+        usergt.establish((err) => {
+
+            usergt.db.disable('user', 'update', { value: new Error('update failed') });
+
+            const username = 'zoelogic';
+            const password = 'paSS-w0rd_4t--t';
+
+            usergt.authenticate(username, password, (err, authenticatedUserRecord) => {
+
+                delete Config.connection.test;
+                usergt.db.enable('user', 'update');
+
+                expect(err.output.statusCode).toBe(500);
+                expect(err.output.payload.error).toBe('Internal Server Error');
+                expect(err.message).toBe('db.user.update - incrementLoginAttempt failed');
+                return usergt.db.close(done);
+            });
+        });
+    });
+
+    // it('Query.user.setLockout db.user.update failure on lockout', (done) => {
+
+    //     // return Query.user.setLockout.call(this, userDocument.id, (err, userDocument) => {
+
+    //     // return usergt.db.close(done);
+
+    // });
 });
